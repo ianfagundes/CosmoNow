dc59727aef76ab91764584f216fc8f222e92d4f5
ianfagundes <ianfag@icloud.com>
2025-02-12 19:28:29 -0300

    [feature - Service] Adiciona CosmoService.
    CosmoService é um serviço assíncrono que usa URLSession para consumir a API, contendo a URL, a chave de acesso e o método de busca dos dados.

--- Data/CosmoService.swift
+++ Data/CosmoService.swift
@@ -5,3 +5,31 @@
 //  Created by Ian Fagundes on 12/02/25.
 //
 
+import Foundation
+
+class CosmoService {
+    private let baseURL: URL
+    private let apikey: String = "c0cfC2fB8BG5hFkAu8x3bHkw2oUzE8xha4RDpfWC"
+
+    init(baseURL: URL = URL(string: "https://api.nasa.gov/planetary/apod")!) {
+        self.base
+    }
+
+    func fetchCosmo(for date: String? = nil) async throws -> CosmoModel {
+        var urlComponents = URLComponents(url: baseURL)!
+        urlComponents.queryItems = [
+            URLQueryItem(name: "api_key", value: apikey),
+        ]
+
+        if let date = date {
+            urlComponents.queryItems?.append(URLQueryItem(name: "date", value: date))
+        }
+
+        guard let url = urlComponents.url else {
+            throw URLError(.badURL)
+        }
+
+        let (data, _) = try await URLSession.shared.data(from: url)
+        return try JSONDecoder().decode(CosmoModel.self, from: data)
+    }
+}
