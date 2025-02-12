9e76dc81839aaf952cd528a4369f1540a673aba8
ianfagundes <ianfag@icloud.com>
2025-02-12 19:30:22 -0300

    [feature - Model] Adiciona cosmoModel.
    CosmoModel é o representação que contém os atributos do objeto JSON que a API deve retornar.

--- Domain/CosmoModel.swift
+++ Domain/CosmoModel.swift
@@ -5,3 +5,21 @@
 //  Created by Ian Fagundes on 12/02/25.
 //
 
+import Foundation
+
+struct CosmoModel: Codable, Identifiable {
+    let id: String { date }
+    let date: String
+    let explanation: String
+    let hdurl: String?
+    let mediaType: String
+    let serviceVersion: String
+    let title: String
+    let url: String
+
+    enum CodingKeys: String, CodingKey {
+        case date, explanation, hdurl, title, url
+        case mediaType = "media_type"
+        case serviceVersion = "service_version"
+    }
+}
