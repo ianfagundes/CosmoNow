77e102a3f81dc4781725e3ed74199e3855038da1
ianfagundes <ianfag@icloud.com>
2025-02-13 00:00:13 -0300

    [feature - Domain] Adiciona implementação do GetCosmoUseCaseProtocol.

--- Domain/GetCosmoUseCase.swift
+++ Domain/GetCosmoUseCase.swift
@@ -5,3 +5,16 @@
 //  Created by Ian Fagundes on 12/02/25.
 //
 
+import Foundation
+
+class GetCosmoUseCase: GetCosmoUseCaseProtocol {
+    private let repository: CosmoServiceProtocol
+
+    init(repository: CosmoServiceProtocol) {
+        self.repository = repository
+    }
+
+    func execute(for date: String?) async throws -> CosmoModel {
+        return try await repository.fetchCosmo(for: date)
+    }
+}
