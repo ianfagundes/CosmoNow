58a3c319bac4de6cf7516ecba941699c28104910
ianfagundes <ianfag@icloud.com>
2025-02-12 23:57:44 -0300

    [fix - Model] Corrige modelo recebido pela API.

--- Domain/CosmoModel.swift
+++ Domain/CosmoModel.swift
@@ -11,14 +11,13 @@ struct CosmoModel: Codable, Identifiable {
     var id: String { date }
     let date: String
     let explanation: String
-    let hdurl: String?
     let mediaType: String
     let serviceVersion: String
     let title: String
     let url: String
 
     enum CodingKeys: String, CodingKey {
-        case date, explanation, hdurl, title, url
+        case date, explanation, title, url
         case mediaType = "media_type"
         case serviceVersion = "service_version"
     }
