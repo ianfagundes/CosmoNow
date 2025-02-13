4f87b4373c34f971c69523dda40f2e9f734f7ee4
ianfagundes <ianfag@icloud.com>
2025-02-12 23:57:02 -0300

    [feature - Domain] adiciona interface para desacoplar ViewModel de CosmoService.
    Simplifica texto MockCosmoService.

---
+++ Domain/GetCosmoUseCaseProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  GetCosmoUseCaseProtocol.swift
+//  CosmoNow
+//
+//  Created by Ian Fagundes on 12/02/25.
+//
+
+import Foundation
+
+protocol GetCosmoUseCaseProtocol {
+    func execute(for date: String?) async throws -> CosmoModel
+}
