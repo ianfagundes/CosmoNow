5b32978c1aef1d9983901605aa89039689c63020
ianfagundes <ianfag@icloud.com>
2025-02-14 12:51:10 -0300

    [update - Resources] Ajustes de layout, no themeManager.

--- Resources/ThemeManager.swift
+++ Resources/ThemeManager.swift
@@ -10,19 +10,25 @@ import SwiftUI
 struct ThemeManager {
     static let backgroundColor: Color = {
         return Color(uiColor: UIColor { traitCollection in
-            traitCollection.userInterfaceStyle == .dark ? .black : .white
+            traitCollection.userInterfaceStyle == .dark ? #colorLiteral(red: 0.039, green: 0.0, blue: 0.137, alpha: 1.0) : #colorLiteral(red: 0.984, green: 0.988, blue: 0.89, alpha: 1.0)
         })
     }()
 
     static let primaryColor: Color = {
         return Color(uiColor: UIColor { traitCollection in
-            traitCollection.userInterfaceStyle == .dark ? UIColor.systemGreen : UIColor.systemPurple
+            traitCollection.userInterfaceStyle == .dark ? #colorLiteral(red: 0.598, green: 0.986, blue: 0.827, alpha: 1.0) : #colorLiteral(red: 0.071, green: 0.306, blue: 0.451, alpha: 1.0)
         })
     }()
 
     static let secondaryColor: Color = {
         return Color(uiColor: UIColor { traitCollection in
-            traitCollection.userInterfaceStyle == .dark ? UIColor(red: 0.6, green: 1.0, blue: 0.3, alpha: 1) : UIColor(red: 0.5, green: 0, blue: 0.8, alpha: 1)
+            traitCollection.userInterfaceStyle == .dark ? #colorLiteral(red: 0.598, green: 0.986, blue: 0.827, alpha: 1.0) : #colorLiteral(red: 0.071, green: 0.306, blue: 0.451, alpha: 1.0)
+        })
+    }()
+    
+    static let textColor: Color = {
+        return Color(uiColor: UIColor { traitCollection in
+            traitCollection.userInterfaceStyle == .dark ? #colorLiteral(red: 0.984, green: 0.988, blue: 0.89, alpha: 1.0) : #colorLiteral(red: 0.039, green: 0.0, blue: 0.137, alpha: 1.0)
         })
     }()
 }
