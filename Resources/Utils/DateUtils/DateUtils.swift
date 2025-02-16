40bb01d921297d377e86e7dd39e0657b24be91bf
ianfagundes <ianfag@icloud.com>
2025-02-15 21:24:30 -0300

    [feature - Utils] Adiciona método para formatar data dia, mês, ano.

--- Resources/Utils/DateUtils/DateUtils.swift
+++ Resources/Utils/DateUtils/DateUtils.swift
@@ -19,4 +19,17 @@ struct DateUtils {
         formatter.dateFormat = "yyyy-MM-dd"
         return formatter.string(from: date)
     }
+    
+    static func formatDate(_ dateString: String) -> String {
+        let inputFormatter = DateFormatter()
+        inputFormatter.dateFormat = "yyyy-MM-dd"
+
+        let outputFormatter = DateFormatter()
+        outputFormatter.dateStyle = .medium
+
+        if let date = inputFormatter.date(from: dateString) {
+            return outputFormatter.string(from: date)
+        }
+        return dateString
+    }
 }
