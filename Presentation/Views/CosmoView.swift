844166f44303699285248db4d0dfdcb30128c45e
ianfagundes <ianfag@icloud.com>
2025-02-15 21:42:02 -0300

    [update - Presentation] Refatora CosmoView fazendo uso do novo componente CosmoDetailView.

--- Presentation/Views/CosmoView.swift
+++ Presentation/Views/CosmoView.swift
@@ -20,52 +20,12 @@ struct CosmoView: View {
     @EnvironmentObject var favoritesViewModel: FavoritesViewModel
     
     var body: some View {
-        VStack(spacing: 16) {
-            if viewModel.isLoading && viewModel.cosmo == nil {
+        VStack {
+            if viewModel.isLoading {
                 ProgressView()
                     .scaleEffect(1.5)
             } else if let cosmo = viewModel.cosmo {
-                VStack(spacing: 5) {
-                    Text(cosmo.title)
-                        .font(.title)
-                        .foregroundColor(ThemeManager.secondaryColor).multilineTextAlignment(.center)
-                        .padding(.horizontal)
-                        .padding(.top, 32)
-
-                    Text(cosmo.date)
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                }
-                ZStack(alignment: .bottomTrailing) {
-                    if cosmo.mediaType == .image {
-                        CachedAsyncImage(
-                            url: cosmo.mediaURL?.absoluteString ?? "",
-                            width: UIScreen.main.bounds.width - 64,
-                            height: 192
-                        )
-                        .cornerRadius(10)
-                        .padding(.top, 5)
-                    } else {
-                        MediaView(mediaType: cosmo.mediaType, mediaURL: cosmo.mediaURL)
-                            .frame(width: UIScreen.main.bounds.width - 64, height: 192)
-                            .cornerRadius(10)
-                            .padding(.top, 5)
-                    }
-
-                    FavoriteButton(
-                        isFavorite: Binding(
-                            get: { favoritesViewModel.isFavorite(CosmoModel(from: cosmo)) },
-                            set: { _ in favoritesViewModel.toggleFavorite(CosmoModel(from: cosmo)) }
-                        )
-                    )
-                }
-                ScrollView {
-                    Text(cosmo.explanation)
-                        .font(.body)
-                        .foregroundColor(ThemeManager.textColor)
-                        .padding(.horizontal, 32)
-                        .padding(.bottom, 48)
-                }
+                CosmoDetailView(cosmo: CosmoModel(from: cosmo), favoritesViewModel: favoritesViewModel)
             } else if let errorMessage = viewModel.errorMessage {
                 VStack {
                     Text(errorMessage)
